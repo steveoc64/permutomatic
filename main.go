ec7d9b1b2ae83da97da20940104fc72c650035df
SteveOC <steven.oconnor@neds.com>
2019-03-15 13:52:20 +1000

    significant length is now 4

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/steveoc64/memdebug"
 	"os"
+	"sort"
 	"time"
 )
 
-func sliceSame(a,b []int) bool {
-	for i := 0; i < len(a); i++ {
+func sliceSame(a, b []int) bool {
+	for i := 0; i < len(a) && i < len(b); i++ {
 		if a[i] != b[i] {
 			return false
 		}
@@ -20,6 +21,9 @@ func permutations(arr []int) [][]int {
 	var helper func([]int, int)
 	res := [][]int{}
 	significantLength := len(arr) / 2
+	if significantLength > 4 {
+		significantLength = 4
+	}
 	t1 := time.Now()
 	memdebug.Print(t1, "output array is uniques of size", significantLength)
 
@@ -36,6 +40,7 @@ func permutations(arr []int) [][]int {
 			for i := 0; i < len(res); i++ {
 				if sliceSame(res[i], tmp) {
 					alreadyThere = true
+					break
 				}
 			}
 			if !alreadyThere {
@@ -46,7 +51,19 @@ func permutations(arr []int) [][]int {
 		} else {
 			flipflop := true
 			for i := 0; i < n; i++ {
-				helper(arr, n-1)
+				alreadyThere := false
+				if false && n == (significantLength-1) {
+					// at this stage we are in danger of dupes, if we are duped, then dont bother diving deeper
+					for j := 0; j < len(res); j++ {
+						if sliceSame(res[j], arr[:n]) {
+							alreadyThere = true
+							break
+						}
+					}
+				}
+				if !alreadyThere {
+					helper(arr, n-1)
+				}
 				if flipflop {
 					arr[0], arr[n-1] = arr[n-1], arr[0]
 					flipflop = false
@@ -58,21 +75,38 @@ func permutations(arr []int) [][]int {
 		}
 	}
 	helper(arr, len(arr))
+
+	// sort the results
+	sort.Slice(res, func(i, j int) bool {
+		for k := 0; k < significantLength; k++ {
+			if res[i][k] < res[j][k] {
+				return true
+			}
+			if res[i][k] > res[j][k] {
+				return false
+			}
+			// else they are equal - try the next digit
+		}
+		return false
+	})
 	return res
 }
 
 func main() {
+	memdebug.Profile()
+	defer memdebug.WriteProfile()
+
 	debug := os.Getenv("QUIET") == ""
 	t1 := time.Now()
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	memdebug.Print(t1, "start", arr)
 	perm := permutations(arr)
-	memdebug.Print(t1, "done permutations", len(perm))
+	memdebug.Print(t1, "reduced permutations to", len(perm))
 	t1 = time.Now()
 	if debug {
 		for i := 0; i < int(len(perm)); i++ {
 			if i > 0 {
-				if i % 16 == 0 {
+				if i%16 == 0 {
 					fmt.Println("")
 				} else {
 					fmt.Print(", ")
@@ -82,7 +116,7 @@ func main() {
 		}
 		fmt.Println("")
 	}
-	memdebug.Print(t1, "done print results", len(perm))
+	memdebug.Print(t1, "done results", len(perm))
 	t1 = time.Now()
 	if false && debug {
 		for i := 0; i < int(len(perm)); i++ {
@@ -93,15 +127,11 @@ func main() {
 					fmt.Print(", ")
 				}
 			}
-			for j := 0; j < len(arr)/2; j++ {
+			for j := 0; j < len(perm[i]); j++ {
 				fmt.Print(perm[i][j])
 			}
 		}
 		fmt.Println("")
 	}
 	memdebug.Print(t1, "all done")
-
-	// simplify results
-
-
 }
